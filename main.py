54f4e05b2fce396991dca7d91b89dadac6316d0e
Fumnanya Obi <cobi43@rrc.ca>
2025-11-24 15:22:35 -0600

    Revert "Completed Part 3"
    This reverts commit 649e49f5b66986b53f002b0c2bd4c7f5647cc18a.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sqlite3
 import pandas as pd
 
-data = pd.read_csv (r'Saskatoon.csv')   
+data = pd.read_csv (r'example.csv')   
 df = pd.DataFrame(data)
 
-connection = sqlite3.connect('Saskatoon.db')
+connection = sqlite3.connect('location.db')
 
 crs = connection.cursor()
 
